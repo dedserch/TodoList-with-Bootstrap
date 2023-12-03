5679ff20388f705900b1b78008bccee2a575babf
dedserch <dedvetal@mail.ru>
2023-12-04 01:01:42 +0300

    The First Commit. Created simple design with Bootstrap and add some logic

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+const task = [
+    {
+        id:1,
+        taskName: "Пойти гулять",
+        isImportant:false,
+        isDone:false
+    }
+]
+
+const submit = document.querySelector(".addSubmit")
+const addAgree = document.querySelector(".addAgree")
+const addNoAgree = document.querySelector(".addNoAgree")
+const addInput = document.querySelector(".addInput")
+
+submit.addEventListener("click", () => {
+    console.log("123");
+})
